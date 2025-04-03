64ddcf34b725687eb60e91c600987e58996ebf21
abdullah <abdullah981066@gmail.com>
2025-04-03 21:14:28 +0530

    Implement authentication logic for sign-up and sign-in with Firebase

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -13,6 +13,9 @@ import Link from "next/link";
 import { toast } from "sonner";
 import FormField from "./FormField";
 import { useRouter } from "next/navigation";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/client";
+import { signIn, signUp } from "@/lib/actions/auth.action";
 
 const authFormSchema = (type: FormType) => {
     return z.object({
@@ -36,15 +39,46 @@ const AuthForm = ({ type }: { type: FormType}) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values);
     try {
         if(type === "sign-up") {
+
+            const { name, email, password } = values
+
+            const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
+
+            const user = await signUp({
+              uid: userCredentials.user.uid,
+              name: name!,
+              email,
+              password,
+            })
+
+            if(!user?.success) {
+                toast.error(user?.message)
+                return
+            }
+
             toast.success('Account created successfully. Please sign in.')
             router.push('/sign-in')
         } else {
+            const { email, password } = values
+
+            const userCredentials = await signInWithEmailAndPassword(auth, email, password)
+
+            const idToken = await userCredentials.user.getIdToken()
+
+            if (!idToken) {
+                toast.error('Invalid credentials. Please try again.')
+                return
+            }
+
+            await signIn({
+              email, idToken
+            })
             toast.success('Sign in successfully.')
             router.push('/')
         }
---
+++ src/lib/actions/auth.action.ts
@@ -0,0 +1,83 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { auth, db } from "../../../firebase/admin";
+
+export async function signUp(params: SignUpParams) {
+  const { uid, name, email } = params;
+
+  try {
+    const userRocord = await db.collection("users").doc(uid).get();
+
+    if (userRocord.exists) {
+      return {
+        success: false,
+        message: "User already exists. Please login.",
+      };
+    }
+
+    await db.collection("users").doc(uid).set({
+      name,
+      email,
+    });
+
+    return {
+        success: true,
+        message: "User created successfully",
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    console.error("Error signing up:", error);
+    if (error.code === "auth/email-already-exists") {
+      return {
+        success: false,
+        message: "Email already exists",
+      };
+    }
+
+    return {
+      success: false,
+      message: "Error signing up",
+    };
+  }
+}
+
+export async function signIn(params: SignInParams) {
+  const { email, idToken } = params;
+
+  try {
+    const userRocord = await auth.getUserByEmail(email);
+
+    if (!userRocord) {
+      return {
+        success: false,
+        message: "User not found. Please sign up.",
+      };
+    }
+
+    await setSessionCookie(idToken);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    console.error("Error signing up:", error);
+    return {
+      success: false,
+      message: "Error signing in",
+    };
+  }
+}
+
+export async function setSessionCookie(idToken: string) {
+  const cookieStore = await cookies();
+
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: 60 * 60 * 24 * 5 * 1000, // 5 days
+  });
+
+  cookieStore.set("session", sessionCookie, {
+    maxAge: 60 * 60 * 24 * 5, // 5 days
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
+}
